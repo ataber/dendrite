d084bcf8f31550ec5ba4e050e19761973c87b693
Andrew Taber <andrew.e.taber@gmail.com>
2016-09-01 12:34:26 -0700

    Use lambdas instead of substitution due to poor subs performance.

--- dendrite/core/operad.py
+++ dendrite/core/operad.py
@@ -8,7 +8,7 @@ from collections import OrderedDict
 class Operad:
   def __init__(self, expression, namespace=None):
     self.expression = expression
-    self.lambda_expression = None
+    self._symbolic_lambda = None
     self.inputs = {}
     self.namespace = namespace
     self.functional_namespace = None
@@ -16,29 +16,24 @@ class Operad:
   def set_inputs(self, **inputs):
     self.inputs = {**inputs, **self.inputs}
 
-  def __call__(self, X=x, Y=y, Z=z, **kwargs):
-    if any([isinstance(c, Tensor) for c in [X,Y,Z]]):
-      return self.compute_tensorflow(X,Y,Z, **kwargs)
-    elif any([isinstance(c, sympy.Basic) for c in [X,Y,Z]]):
-      return substitute(self.substitute_symbols(), {"x": X, "y": Y, "z": Z})
-    else:
-      return self.symbolic_lambda(X,Y,Z)
+  def __call__(self, X=x, Y=y, Z=z, scope=None, functional_scope=None):
+    if any([isinstance(c, Tensor) for c in [X, Y, Z]]):
+      return self.compute_tensorflow(X, Y, Z, scope=scope, functional_scope=functional_scope)
+    return self.symbolic_lambda(X, Y, Z)
 
   @property
   def symbolic_lambda(self):
-    if self.lambda_expression is None:
-      self.lambda_expression = sympy.lambdify([x,y,z], self.substitute_symbols())
-    return self.lambda_expression
+    if self._symbolic_lambda is None:
+      self._symbolic_lambda = sympy.lambdify([x,y,z], self.substitute_symbols(), "sympy")
+    return self._symbolic_lambda
 
   def substitute_symbols(self):
     subs = {}
 
     if type(self.expression) == sympy.Subs:
       gx, gy, gz = self.inputs["g"].substitute_symbols()
-      subs["gx"] = gx
-      subs["gy"] = gy
-      subs["gz"] = gz
-      subs["f"] = self.inputs["f"].substitute_symbols()
+      f = self.inputs["f"]
+      return f(gx, gy, gz)
     else:
       for name, value in self.inputs.items():
         if isinstance(value, Operad):
@@ -48,47 +43,36 @@ class Operad:
 
     return substitute(self.expression, subs)
 
-  def compute_tensorflow(self, X, Y, Z, **kwargs):
-    namespace = self.namespace or kwargs.get("scope", None)
+  def compute_tensorflow(self, X, Y, Z, scope=None, functional_scope=None):
+    namespace = self.namespace or scope
 
-    if self.functional_namespace and kwargs.get("functional_scope", None):
-      functional_namespace = kwargs["functional_scope"] + self.functional_namespace
+    if self.functional_namespace and functional_scope:
+      functional_namespace = functional_scope + self.functional_namespace
     else:
-      functional_namespace = self.functional_namespace or kwargs.get("functional_scope", None)
+      functional_namespace = self.functional_namespace or functional_scope
 
     with name_scope(None):
       # Clear scope, otherwise functional components end up nested within Operations
       with name_scope(functional_namespace) as functional_scope:
         with name_scope(namespace) as scope:
-          kwargs["functional_scope"] = functional_scope
-          kwargs["scope"] = scope
+          if type(self.expression) == sympy.Subs:
+            gx, gy, gz = self.inputs["g"](X, Y, Z, scope=scope, functional_scope=functional_scope)
+            f = self.inputs["f"]
+            return f(gx, gy, gz, scope=scope, functional_scope=functional_scope)
 
           input_tensors = OrderedDict()
-
-          if type(self.expression) == sympy.Subs:
-            gx, gy, gz = self.inputs["g"](X,Y,Z, **kwargs)
-            input_tensors["gx"] = gx
-            input_tensors["gy"] = gy
-            input_tensors["gz"] = gz
-
-            f = self.inputs["f"]
-            return f(gx, gy, gz, **kwargs)
-
           for name, value in self.inputs.items():
             if isinstance(value, Operad):
-              input_tensors[name] = value(X,Y,Z, **kwargs)
+              input_tensors[name] = value(X, Y, Z, scope=scope, functional_scope=functional_scope)
+            elif isinstance(value, sympy.Basic):
+              input_tensors[name] = sympy.lambdify([x, y, z], value, "tensorflow")(X, Y, Z)
             else:
               input_tensors[name] = convert_to_tensor(value, dtype=float32)
 
-          variable_list = [x,y,z] + list(input_tensors.keys())
+          variable_list = [x, y, z] + list(input_tensors.keys())
 
           tf_lambda = sympy.lambdify(variable_list, self.expression, "tensorflow")
-          return tf_lambda(X,Y,Z,*input_tensors.values())
-
-  def wrap_output(self, func):
-    def wrapped(*args, **kwargs):
-      return func(self(*args, **kwargs))
-    return self.__class__(wrapped, inputs=[self])
+          return tf_lambda(X, Y, Z, *input_tensors.values())
 
   def __lshift__(self, other):
     raise ValueError("Can not compose types: %s and %s" % (type(self), type(other)))
@@ -99,7 +83,5 @@ class Operad:
 def substitute(expr, subs):
   if isinstance(expr, tuple):
     return sympy.Tuple(*[substitute(e, subs) for e in expr])
-  substituted = expr.subs(subs)
-  if type(substituted) == sympy.Subs:
-    return substituted.doit()
+  substituted = sympy.lambdify(subs.keys(), expr, "sympy")(**subs)
   return substituted
