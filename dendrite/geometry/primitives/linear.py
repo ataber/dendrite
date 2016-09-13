66908c986ac0fa14d17cdeee78fa69a23943f5d7
Andrew Taber <andrew.e.taber@gmail.com>
2016-09-13 12:29:18 -0700

    Normalize dat plane.

--- dendrite/geometry/primitives/linear.py
+++ dendrite/geometry/primitives/linear.py
@@ -1,8 +1,10 @@
-from numpy import ndarray
+from numpy import ndarray, linalg
 from sympy.abc import x,y,z
 from dendrite.core.functional import Functional as F
 from dendrite.core.expression import Expression as E
 
 @E
-def plane(a, b, c, offset) -> F:
+def plane(normal: ndarray, offset) -> F:
+  normal = normal / linalg.norm(normal)
+  a, b, c = normal
   return x*a + b*y + c*z + offset
